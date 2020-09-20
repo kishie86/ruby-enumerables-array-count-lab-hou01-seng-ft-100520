3edf5125a4392de23ab1ade51eb55634a12a4cc7
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-20 23:12:41 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -7,7 +7,7 @@ end
 
 def count_empty_strings(array)
   array.count do |element|
-    element.class == ""
+    element == ""
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
 end
